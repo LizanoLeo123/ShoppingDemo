c6dd1ab851040327e8a5e5e6d55e1092e1fc5f63
Leo Lizano <leoln123@live.com>
2023-07-26 19:42:57 -0600

    Created basic top down character movement

---
+++ GAME/BG-ShoppingDemo/Assets/GAME/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    void Update()
+    {
+        //Input
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+    }
+}
